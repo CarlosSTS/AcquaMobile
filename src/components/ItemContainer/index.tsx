4f283efe1dd2db096da80178398167a9761c6c49
Carlos Ferreira <carlossts826@gmail.com>
2020-11-26 17:44:51 -0300

    Keyboard completed

--- src/components/ItemContainer/index.tsx
+++ src/components/ItemContainer/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Feather from "react-native-vector-icons/Feather";
 
 import { useNavigation, useRoute } from "@react-navigation/native";
-import {ScrollView , StyleSheet, View, Text,Linking } from "react-native";
+import {ScrollView , StyleSheet, View, Text,Linking,Keyboard } from "react-native";
 import {  RectButton } from "react-native-gesture-handler";
 
 interface clientDetailRouteParams {
@@ -18,8 +18,9 @@ export default function ItemContainer() {
   const navigation = useNavigation();
 
   const params = route.params as clientDetailRouteParams;
-  const message = `olá ${params.full_name}, estou estrando em contato pois gostaria de saber mais informações sobre seu pedido`
+  const message = `olá ${params.full_name}, estou estrando em contato pois gostaria de saber mais informações sobre seu pedido`;
 
+  const [isKeyBoardVisible, setBoardVisible] =useState(false);
   function NavigationToClientDetail(id: number) {
     navigation.navigate('ClientStackRoutes',{
       screen :'ClientDetail',
@@ -65,54 +66,78 @@ export default function ItemContainer() {
   function sendWhatsapp() {
     Linking.openURL(`whatsapp://send?phone=+55${params.phone}&text=${message}`)
       }
+useEffect (()=> {
+  const KeyboardDidShowListener = Keyboard.addListener(
+    'keyboardDidShow',
+     () => {
+      setBoardVisible(true);
+    }
+  );
+  const KeyboardDidHideListener = Keyboard.addListener(
+    'keyboardDidHide',
+     () => {
+      setBoardVisible(false);
+      }
+    );
+return () => {
+  KeyboardDidShowListener.remove();
+  KeyboardDidHideListener.remove();
+};
+},[]);
 
   return (
     <>
+{isKeyBoardVisible ? (
+<>
+</>
 
-      <View style={styles.itemsContainer}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+): (
 
-        <RectButton style={styles.item} onPress={() =>NavigationToClientDetail(params.id)}>
-            <Feather color="#000" name="user" size={42} />
-            <Text style={styles.itemTitle}>Ir para Clientes</Text>
-          </RectButton>
+  <View style={styles.itemsContainer}>
+  <ScrollView horizontal showsHorizontalScrollIndicator={false}>
 
-          <RectButton style={styles.item} onPress={() => NavigationToPurchaseDetail(params.id)}>
-            <Feather color="#e82041" name="shopping-cart" size={42} />
-            <Text style={styles.itemTitle}>Ir para Compras</Text>
-          </RectButton>
+  <RectButton style={styles.item} onPress={() =>NavigationToClientDetail(params.id)}>
+      <Feather color="#000" name="user" size={42} />
+      <Text style={styles.itemTitle}>Ir para Clientes</Text>
+    </RectButton>
 
-          <RectButton style={styles.item} onPress={() => NavigationToSaleDetail(params.id)}>
-            <Feather color="orange" name="shopping-bag" size={42} />
-            <Text style={styles.itemTitle}>Ir para vendas</Text>
-          </RectButton>
+    <RectButton style={styles.item} onPress={() => NavigationToPurchaseDetail(params.id)}>
+      <Feather color="#e82041" name="shopping-cart" size={42} />
+      <Text style={styles.itemTitle}>Ir para Compras</Text>
+    </RectButton>
 
-          <RectButton style={styles.item} onPress={() => NavigationToCarboyLoanDetail(params.id)}>
-            <FontAwesome color="#000" name="handshake-o" size={42} />
-            <Text style={styles.itemTitle}>Ir para Empréstimos</Text>
-          </RectButton>
+    <RectButton style={styles.item} onPress={() => NavigationToSaleDetail(params.id)}>
+      <Feather color="orange" name="shopping-bag" size={42} />
+      <Text style={styles.itemTitle}>Ir para vendas</Text>
+    </RectButton>
 
-          <RectButton style={styles.item} onPress={() =>NavigationToRepots(params.id)}>
-            <FontAwesome color="brown" name="folder" size={42} />
-            <Text style={styles.itemTitle}>Ir para Relatórios</Text>
-          </RectButton>
+    <RectButton style={styles.item} onPress={() => NavigationToCarboyLoanDetail(params.id)}>
+      <FontAwesome color="#000" name="handshake-o" size={42} />
+      <Text style={styles.itemTitle}>Ir para Empréstimos</Text>
+    </RectButton>
 
-          <RectButton style={styles.item} onPress={() => NavigationToMoveDetail(params.id)}>
-            <Feather color="#333666" name="repeat" size={42} />
-            <Text style={styles.itemTitle}>Ir para Movimentos</Text>
-          </RectButton>
+    <RectButton style={styles.item} onPress={() =>NavigationToRepots(params.id)}>
+      <FontAwesome color="brown" name="folder" size={42} />
+      <Text style={styles.itemTitle}>Ir para Relatórios</Text>
+    </RectButton>
 
-          <RectButton style={styles.item} onPress={() => NavigationToClientRouteDetail(params.id)}>
-            <Feather color="black" name="truck" size={42} />
-            <Text style={styles.itemTitle}>Ir para Rotas</Text>
-          </RectButton>
+    <RectButton style={styles.item} onPress={() => NavigationToMoveDetail(params.id)}>
+      <Feather color="#333666" name="repeat" size={42} />
+      <Text style={styles.itemTitle}>Ir para Movimentos</Text>
+    </RectButton>
 
-          <RectButton style={styles.item} onPress={sendWhatsapp}>
-            <FontAwesome color="#34CB79" name="whatsapp" size={42} />
-            <Text style={styles.itemTitle}>Ir para whatsapp</Text>
-          </RectButton>
-        </ScrollView>
-      </View>
+    <RectButton style={styles.item} onPress={() => NavigationToClientRouteDetail(params.id)}>
+      <Feather color="black" name="truck" size={42} />
+      <Text style={styles.itemTitle}>Ir para Rotas</Text>
+    </RectButton>
+
+    <RectButton style={styles.item} onPress={sendWhatsapp}>
+      <FontAwesome color="#34CB79" name="whatsapp" size={42} />
+      <Text style={styles.itemTitle}>Ir para whatsapp</Text>
+    </RectButton>
+  </ScrollView>
+ </View>
+)}
 
     </>
   )
